3099f86cd607b883e3ac9979eef2fc721b63a3e6
nic00la1 <NicolaKaleta@TE-LOD-D-09S209>
2026-01-13 11:07:54 +0100

    zmiany

--- SaveFileApp/MainPage.xaml.cs
+++ SaveFileApp/MainPage.xaml.cs
@@ -1,6 +1,6 @@
 ﻿using System.Windows.Input;
 using System.Text;
-using CommunityToolkit.Maui;
+using CommunityToolkit.Maui.Storage;
 
 
 namespace SaveFileApp
@@ -8,21 +8,36 @@ namespace SaveFileApp
     public partial class MainPage : ContentPage
     {
         public ICommand openSaveFile;
+        public ICommand selectFileBtn;
+
         public MainPage()
         {
             InitializeComponent();
-            openSaveFile = new Command<Task>(SaveFile);
+            openSaveFile = new Command(async() => await SaveFile());
+
+            Przycisk.Command = openSaveFile;
+            Przycisk2.Command = selectFileBtn;
+        }
+
+        public async Task SelectFile()
+        {
+            var filePicker = await FilePicker.Default.PickAsync(new PickOptions 
+            {
+                PickerTitle = "Wybierz plik",
+                FileTypes = FilePickerFileType.Images
+            });
+            if (filePicker != null)
+                await DisplayAlertAsync("Twój plik", $"Wybrany plik to: {filePicker.FullPath}", "OK");
         }
 
         public async Task SaveFile()
         {
-            var stream = new MemoryStream(UTF8Encoding.UTF8.GetBytes("To jest nasz plik!"));
+            var stream = new MemoryStream(Encoding.Default.GetBytes("To jest nasz plik!"));
             using var filePicker = await FileSaver.Default.SaveAsync("text.txt", stream);
             if (filePicker.IsSuccessful)
                 await DisplayAlertAsync("Tytuł", "Udało się", "OK");
             else
                 await DisplayAlertAsync("Tytuł", "Nie udało się", "OK");
-
         }
     }
 }
--- SaveFileApp/MauiProgram.cs
+++ SaveFileApp/MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using CommunityToolkit.Maui;
+using CommunityToolkit.Maui.Storage;
 using Microsoft.Extensions.Logging;
 
 namespace SaveFileApp
@@ -16,7 +17,6 @@ namespace SaveFileApp
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
-
 #if DEBUG
     		builder.Logging.AddDebug();
 #endif
